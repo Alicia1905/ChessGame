9060dc816f406853676aba5148b7d4afdec04e93
khaoula-dot <khaoulaaissaoui121@gmail.com>
2026-03-04 18:10:57 +0000

    Ajout interface UI du jeux

--- core/src/main/java/io/github/some_example_name/Main.java
+++ core/src/main/java/io/github/some_example_name/Main.java
@@ -1,19 +1,17 @@
 package io.github.some_example_name;
 
 import com.badlogic.gdx.Game;
-import io.github.some_example_name.screens.GameScreen;
+import io.github.some_example_name.screens.MenuScreen;
 import io.github.some_example_name.utils.Assets;
 
 public class Main extends Game {
+
     public Assets assets;
 
     @Override
     public void create() {
-        assets = new Assets();
-        assets.load();
-        assets.finishLoading();
-
-        setScreen(new GameScreen(this));
+        assets = new Assets();     // ton Assets.java existe déjà
+        setScreen(new MenuScreen(this)); // on démarre par le menu
     }
 
     @Override
---
+++ core/src/main/java/io/github/some_example_name/managers/AudioManager.java
@@ -0,0 +1,14 @@
+package io.github.some_example_name.managers;
+
+public class AudioManager {
+
+    private static float volume = 0.7f;
+
+    public static void setVolume(float v) {
+        volume = Math.max(0f, Math.min(1f, v));
+    }
+
+    public static float getVolume() {
+        return volume;
+    }
+}
